0417c037e4be2aa9f71fd241017643ad67d77b36
ndewansh <142912662+ndewansh@users.noreply.github.com>
2025-09-23 23:23:41 +0530

    Create lru-cache-using-linked-hashmap.java

---
+++ lru-cache-using-linked-hashmap.java
@@ -0,0 +1,29 @@
+// https://leetcode.com/problems/lru-cache/
+
+class LRUCache {
+    private LinkedHashMap<Integer, Integer> map;
+
+    public LRUCache(int capacity) {
+        map = new LinkedHashMap<Integer, Integer>(capacity, 0.75f, true){
+            @Override
+            protected boolean removeEldestEntry(Map.Entry<Integer, Integer> entry) {
+                return size() > capacity;
+            }
+        };
+    }
+    
+    public int get(int key) {
+        return map.getOrDefault(key, -1);
+    }
+    
+    public void put(int key, int value) {
+        map.put(key, value);
+    }
+}
+
+/**
+ * Your LRUCache object will be instantiated and called as such:
+ * LRUCache obj = new LRUCache(capacity);
+ * int param_1 = obj.get(key);
+ * obj.put(key,value);
+ */
